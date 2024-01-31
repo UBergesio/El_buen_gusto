804ce6471e92d511e5cff066bd2acaaca986dd4f
UBergesio <ulibergesio@gmail.com>
2024-01-31 20:04:56 -0300

    arreglos

--- client/buen-gusto/src/components/Home/Home.jsx
+++ client/buen-gusto/src/components/Home/Home.jsx
@@ -2,6 +2,8 @@ import Carrusel from "../Carrusel/Carrusel";
 import Nosotros from "../Nosotros/Nosotros";
 import Servicios from "../Servicios/Servicios";
 import Valores from "../Valores/Valores";
+import ContactForm from "../ContactForm/ContactForm"
+import WhatsappButton from "../WhatsappButton/WhatsappButton"
 
 const Home = () => {
   return (
@@ -10,7 +12,7 @@ const Home = () => {
       <Nosotros />
       <Servicios />
       <Valores />
-
+      <WhatsappButton/>
       <ContactForm />
     </div>
   );
